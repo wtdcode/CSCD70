2f5c6fa926d0326a935ce36fcad05e6d7a7b372b
mio <ziqiaokong@outlook.com>
2020-09-27 14:28:06 +0800

    Import assignment 1

---
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/Build.mk
@@ -0,0 +1,17 @@
+OPTIMIZER := FunctionInfo.so
+
+LLVM_VERSION ?= 6.0
+
+CXXFLAGS := $(shell llvm-config-$(LLVM_VERSION) --cxxflags)
+
+OPT_OBJs := $(subst .cpp,.o,$(wildcard *.cpp))
+
+all: $(OPTIMIZER) 
+
+# Build the optimizer from the source files.
+$(OPTIMIZER): $(OPT_OBJs)
+	$(CXX) -dylib -shared $^ -o $@
+
+.PHONY: clean
+clean:
+	$(RM) $(OPTIMIZER) $(OPT_OBJs)
