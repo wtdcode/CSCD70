1ac6bbb0dc4001c3c2ad02323137d95eb1a0c0e8
mio <ziqiaokong@outlook.com>
2020-09-27 14:59:06 +0800

    First version of Function Infomation

--- Assignment1-Introduction_to_LLVM/FunctionInfo/FunctionInfo.cpp
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/FunctionInfo.cpp
@@ -1,6 +1,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
@@ -31,6 +32,28 @@ public:
 
 		// @TODO Please complete this function.
 		
+		for(auto it = M.begin(); it != M.end(); it++){
+			std::size_t instruction_count = 0;
+			std::size_t arguments_count = 0;
+			std::size_t bblocks_count = 0;
+			std::size_t calls_count = 0;
+			outs() << "Name: " << it->getName() << "\n" \
+				   << "ID: " << it->getGlobalIdentifier() << "\n";
+			instruction_count = it->getInstructionCount();
+			for(auto it2 = it->arg_begin(); it2 != it->arg_end(); it2 ++)
+				arguments_count += 1;
+			auto& bblist = it->getBasicBlockList();
+			bblocks_count = bblist.size();
+			for(auto instr = it->begin(); instr != it->end(); instr++)
+				if(auto callistr = dyn_cast<CallInst>(instr))
+					calls_count +=1;
+			outs() << "Args: " << arguments_count << "\n" \
+			   << "Calls: " << calls_count << "\n" \
+			   << "Blocks: " << bblocks_count << "\n" \
+			   << "Insts: "  << instruction_count << "\n";
+		}
+		
+
 		return false;
 	}
 };
